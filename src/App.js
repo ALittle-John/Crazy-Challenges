a88d11171a5766bd04842add53410fc3f6341299
ALittle-John <joaopsousaqi@gmail.com>
2023-02-08 16:15:27 -0300

    feat: Basic build fot now

---
+++ src/App.js
@@ -0,0 +1,17 @@
+import React from "react";
+import { StyleSheet } from "react-native";
+import Level from "./components/Pages/Level/Level";
+
+<Level style={style.App}/>
+
+
+
+
+const style = StyleSheet.create({
+    App: {
+        backgroundColor: '#77ABC7' 
+       ,flexGrow: 1
+       ,justifyContent: "center"
+       ,alignItems: "center"
+       ,padding: 20
+    }})
